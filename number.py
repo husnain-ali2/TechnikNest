748e887f5ca25fa3c2b5879ffa9baf4a84240ce6
Husnain Ali <129890731+husnain-ali2@users.noreply.github.com>
2025-06-26 21:31:58 +0500

    Add files via upload

---
+++ number.py
@@ -0,0 +1,5 @@
+# This script takes two numbers as input from the user and prints their sum.
+num1=float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+result = num1 +num2
+print("The sum of", num1, "and", num2, "is:", result)
